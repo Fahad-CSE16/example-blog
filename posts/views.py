a96a07af7e1ce6edf78d680c2ff0551e493b7cff
Fahad-CSE16 <Fahad-CSE16>
2020-11-09 21:28:46 +0600

    Phase 2 done

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 
+import json
+import requests
 
 # POSTS VIEW ENDPOINT
-def posts(request):
-    return render(request, 'blog-listing.html')
+def posts(request): 
+    api_request=requests.get("https://jsonplaceholder.typicode.com/posts")
+    try:
+        api=json.loads(api_request.content)
+    except Exception as e:
+        api= "error"
+    return render(request, 'blog-listing.html',{'api':api})
 
 
 # POST DETAILS VIEW ENDPOINT
-def post_details(request):
-    return render(request, 'blog-post.html')
+def post_details(request,id):
+    api_request=requests.get(f"https://jsonplaceholder.typicode.com/posts/{id}")
+    comments=requests.get(f"https://jsonplaceholder.typicode.com/posts/{id}/comments")
+    try:
+        api=json.loads(api_request.content)
+        comment=json.loads(comments.content)
+    except Exception as e:
+        api= "error"
+        comment= "error"
+    return render(request, 'blog-post.html',{'api':api,'comment':comment})
